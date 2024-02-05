9178ea45cf2b0cd34ec411f3d7bce39a5d567f6e
tomohiro ueno <hagurono9@gmail.com>
2024-02-05 09:27:10 +0900

    add

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_back_golang/widgets/app_button.dart';
+import 'package:flutter_back_golang/widgets/app_logo.dart';
 
 class TopPage extends StatefulWidget {
   const TopPage({super.key});
@@ -15,6 +17,18 @@ class _TopPageState extends State<TopPage> {
           child: Container(
         padding: const EdgeInsets.all(16),
         height: 300,
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          const AppLogo(),
+          const Text('ようこそ！スーパーチャットへ'),
+          const Text('ログインして始めよう！'),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AppButton(width: 80, onPressed: () {}, text: 'ログイン'),
+              AppButton(width: 80, onPressed: () {}, text: '新規登録')
+            ],
+          )
+        ]),
       )),
     );
   }
