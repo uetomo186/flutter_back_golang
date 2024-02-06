9eda71cc56c4485beab99f594b01bb7a6226ec5b
uetomo186 <hagurono9@gmail.com>
2024-02-06 18:18:36 +0900

    ログインページを作成しました。

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.green,
       ),
       home: const TopPage(),
     );
