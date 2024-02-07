f757e3986d1e1bde7291b1f06780efd45bf3849a
tomohiro ueno <hagurono9@gmail.com>
2024-02-08 00:53:44 +0900

    チャットルームを更新

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -6,28 +6,62 @@ import 'package:flutter_back_golang/widgets/message_text_field.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+  const ChatPage({
+    super.key,
+  });
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  late String userId;
+  late ChatRoom room;
+
+  @override
+  void initState() {
+    userId = Supabase.instance.client.auth.currentUser!.id;
+    room =
+        ChatRoom(userId: userId, name: 'New chat', createdAt: DateTime.now());
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Chat Page'),
-        ),
-        drawer: const AppDrawer(),
-        body: Column(
-          children: [
-            Expanded(
-              child: MessageList(),
-            ),
-            const MessageTextField(),
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('Chat Page'),
+      ),
+      drawer: const AppDrawer(),
+      body: Column(
+        children: [
+          Expanded(
+            child: MessageList(),
+          ),
+          MessageTextField(
+            onSubmitted: (message) async {
+              // チャットルームがなければ作成
+              if (room.id == null) {
+                final result = await createChatRoom(context,
+                    userId: room.userId, roomName: room.name);
+                // チャットルームが作成できたら、roomに代入
+                if (result != null) {
+                  setState(() {
+                    room = result;
+                  });
+                }
+              }
+
+              // メッセージテーブルにレコードを挿入
+              if (context.mounted)
+                await sendChatMessage(context,
+                    message: message, room: room, fromBot: false);
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   Future<ChatRoom?> createChatRoom(
@@ -54,13 +88,13 @@ class _ChatPageState extends State<ChatPage> {
 
   Future<void> sendChatMessage(
     BuildContext context, {
-    required String roomId,
     required String message,
+    required ChatRoom room,
     required bool fromBot,
   }) async {
     try {
       await Supabase.instance.client.from('chat_messages').insert({
-        'room_id': roomId,
+        'room_id': room.id,
         'message': message,
         'sent_by_bot': fromBot,
       });
@@ -71,7 +105,7 @@ class _ChatPageState extends State<ChatPage> {
           backgroundColor: Colors.red,
         ),
       );
-      return null;
     }
+    return;
   }
 }
