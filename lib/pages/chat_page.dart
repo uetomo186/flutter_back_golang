60460ca13ac124e8fc388e96b638d387ef77ad89
tomohiro ueno <hagurono9@gmail.com>
2024-02-08 00:41:06 +0900

    add

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_back_golang/models/chat_room.dart';
 import 'package:flutter_back_golang/widgets/app_drawer.dart';
 import 'package:flutter_back_golang/widgets/message_list.dart';
 import 'package:flutter_back_golang/widgets/message_text_field.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -26,5 +28,50 @@ class _ChatPageState extends State<ChatPage> {
             const MessageTextField(),
           ],
         ));
+  }
+
+  Future<ChatRoom?> createChatRoom(
+    BuildContext context, {
+    required String roomName,
+    required String userId,
+  }) async {
+    try {
+      final result = await Supabase.instance.client.from('chat_rooms').insert({
+        'room_name': roomName,
+        'user_id': userId,
+      }).select();
+      return ChatRoom.fromJson(result.first);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return null;
+    }
+  }
+
+  Future<void> sendChatMessage(
+    BuildContext context, {
+    required String roomId,
+    required String message,
+    required bool fromBot,
+  }) async {
+    try {
+      await Supabase.instance.client.from('chat_messages').insert({
+        'room_id': roomId,
+        'message': message,
+        'sent_by_bot': fromBot,
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return null;
+    }
   }
 }
