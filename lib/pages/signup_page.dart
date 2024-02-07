1483ca525084d3b51171ebe3b366ef3f66582243
uetomo186 <hagurono9@gmail.com>
2024-02-07 10:34:48 +0900

    バリデーター関連を追加

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_back_golang/pages/login_page.dart';
 import 'package:flutter_back_golang/widgets/app_button.dart';
 import 'package:flutter_back_golang/widgets/app_logo.dart';
 import 'package:flutter_back_golang/widgets/message_text_field.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -12,6 +13,19 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailControler = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailControler.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,25 +48,58 @@ class _SignupPageState extends State<SignupPage> {
                   ),
                 ),
                 const SizedBox(height: 24),
-                const Form(
+                Form(
+                  key: _formKey,
                   child: Column(
                     children: [
-                      AppTextFormField(labelText: 'メールアドレス'),
-                      SizedBox(height: 24),
                       AppTextFormField(
+                        controller: _emailControler,
+                        labelText: 'メールアドレス',
+                        // 空欄の場合にエラーを表示する
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'メールアドレスを入力してください';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 24),
+                      AppTextFormField(
+                        controller: _passwordController,
                         labelText: 'パスワード',
+                        // 空欄の場合にエラーを表示する
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'パスワードを入力してください';
+                          }
+                          return null;
+                        },
                         obscureText: true,
                       ),
-                      SizedBox(height: 24),
+                      const SizedBox(height: 24),
                       AppTextFormField(
+                        controller: _confirmPasswordController,
                         labelText: 'もう一度パスワードを入力',
                         obscureText: true,
+                        // パスワードが一致しない場合エラーを表示する
+                        validator: (value) {
+                          if (value != _passwordController.text) {
+                            return 'パスワードが一致しません';
+                          }
+                          return null;
+                        },
                       )
                     ],
                   ),
                 ),
                 const SizedBox(height: 24),
-                AppButton(height: 48, onPressed: () async {}, text: '続ける'),
+                AppButton(
+                    height: 48,
+                    onPressed: () async {
+                      // バリデーションエラーがある場合、処理を中断する
+                      if (!_formKey.currentState!.validate()) return;
+                    },
+                    text: '続ける'),
                 const SizedBox(height: 24),
                 Row(
                   mainAxisSize: MainAxisSize.min,
@@ -74,5 +121,14 @@ class _SignupPageState extends State<SignupPage> {
         ),
       ),
     );
+  }
+
+  Future<dynamic> _signup({
+    required String email,
+    required String password,
+  }) async {
+    // ここにサインアップ処理を実装
+    return await Supabase.instance.client.auth
+        .signUp(email: email, password: password);
   }
 }
