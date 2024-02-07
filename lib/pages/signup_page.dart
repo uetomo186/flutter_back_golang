001e4ff858007847749012a0e2a6e0be8a35350d
uetomo186 <hagurono9@gmail.com>
2024-02-07 09:52:58 +0900

    ログイン画面を作成

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_back_golang/widgets/app_button.dart';
+import 'package:flutter_back_golang/widgets/app_logo.dart';
+import 'package:flutter_back_golang/widgets/message_text_field.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -10,9 +13,63 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child:  Text('サインアップページ'),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const AppLogo(),
+                const SizedBox(
+                  height: 24,
+                ),
+                const Text(
+                  'アカウントを作成する',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                const Form(
+                  child: Column(
+                    children: [
+                      AppTextFormField(labelText: 'メールアドレス'),
+                      SizedBox(height: 24),
+                      AppTextFormField(
+                        labelText: 'パスワード',
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 24),
+                      AppTextFormField(
+                        labelText: 'もう一度パスワードを入力',
+                        obscureText: true,
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+                AppButton(height: 48, onPressed: () async {}, text: '続ける'),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text('すでにアカウントをお持ちですか？'),
+                    const SizedBox(width: 8),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('ログイン'),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
