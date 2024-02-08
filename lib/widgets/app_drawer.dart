3baca09e815e8a606ffeb57aa966d5a2d989304a
uetomo186 <hagurono9@gmail.com>
2024-02-08 12:16:49 +0900

    チャットルームの取得を更新

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_back_golang/models/chat_room.dart';
+import 'package:flutter_back_golang/pages/chat_page.dart';
 import 'package:flutter_back_golang/pages/login_page.dart';
+import 'package:flutter_back_golang/widgets/app_drawer_list_tile.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AppDrawer extends StatefulWidget {
@@ -15,7 +18,35 @@ class _AppDrawerState extends State<AppDrawer> {
     return Drawer(
       child: Column(
         children: [
-          const Spacer(),
+          ListTile(
+              leading: const Icon(Icons.add),
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => const ChatPage()));
+              },
+              title: const Text(
+                '新しいチャットルームを作成',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          Expanded(
+            child: StreamBuilder(
+              stream: _getRoomStream(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return const Text('エラーが発生しました');
+                }
+                final rooms = snapshot.data ?? [];
+                return ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    for (final room in rooms) AppDrawerListTile(room: room)
+                  ],
+                );
+              },
+            ),
+          ),
           // logout button
           ListTile(
             title: TextButton(
@@ -58,5 +89,17 @@ class _AppDrawerState extends State<AppDrawer> {
       );
       return false;
     }
+  }
+
+  Stream<List<ChatRoom>> _getRoomStream() {
+    final userId = Supabase.instance.client.auth.currentUser!.id;
+    return Supabase.instance.client
+        .from('chat_rooms')
+        .stream(primaryKey: ['room_id'])
+        .eq('user_id', userId)
+        .order('created_at')
+        .map((snapshot) {
+          return snapshot.map(ChatRoom.fromJson).toList();
+        });
   }
 }
